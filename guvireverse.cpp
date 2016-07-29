455254d6b734598d9f46fda7d5d43417bc1f389b
tauseef95 <tauseef.95@gmail.com>
2016-07-29 21:33:45 +0530

    Add files via upload

---
+++ guvireverse.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  string name;
+  getline(cin, name);
+  reverse(name.begin(), name.end());
+  cout<<name;
+}
